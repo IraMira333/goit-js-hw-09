2497be10026901d7835b12fd3ece1a778af741e7
IraMira333 <irinat333@gmail.com>
2023-05-07 13:21:52 +0300

    Update 03-promises.js

--- src/js/03-promises.js
+++ src/js/03-promises.js
@@ -21,27 +21,32 @@ function onCreatePromiseBtn(evt) {
     position += 1;
     if (position > amount) {
       clearInterval(intervalId);
+      refs.delayInputEl.value = '';
+      refs.stepInputEl.value = '';
+      refs.amountInputEl.value = '';
+      position = 0;
       return;
     }
     delayPlusStep += step * (position - 1);
+    createPromise(position, delayPlusStep)
+      .then(({ position, delay }) => {
+        Notify.success(`✅ Fulfilled promise ${position} in ${delay}ms`);
+      })
+      .catch(({ position, delay }) => {
+        Notify.failure(`❌ Rejected promise ${position} in ${delay}ms`);
+      });
     console.log(position, delayPlusStep);
   }, delayPlusStep);
   console.log(delay, step, amount, position);
 }
 
-// function createPromise(position, delay) {
-//   const shouldResolve = Math.random() > 0.3;
-//   if (shouldResolve) {
-//     // Fulfill
-//   } else {
-//     // Reject
-//   }
-// }
-
-// createPromise(2, 1500)
-//   .then(({ position, delay }) => {
-//     console.log(`✅ Fulfilled promise ${position} in ${delay}ms`);
-//   })
-//   .catch(({ position, delay }) => {
-//     console.log(`❌ Rejected promise ${position} in ${delay}ms`);
-//   });
+function createPromise(position, delay) {
+  return new Promise((resolve, reject) => {
+    const shouldResolve = Math.random() > 0.3;
+    if (shouldResolve) {
+      resolve({ position, delay });
+    } else {
+      reject({ position, delay });
+    }
+  });
+}
